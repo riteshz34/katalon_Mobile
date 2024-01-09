a7e853ed27f7553baff3f503cd4a2a16d2201fcd
riteshzingre <riteshzingre@localhost>
2024-01-09 14:53:49 +0530

    hii

---
+++ Test Suites/smokeTest.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>smokeTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>riteshzingre26@gmail.com;</mailRecipient>
+   <numberOfRerun>2</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>485d7211-512f-4825-be25-914f0d56ce87</testSuiteGuid>
+   <testCaseLink>
+      <guid>4624eb89-501d-4ed2-ade5-590c20ab3169</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_test1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>76a63800-eae9-4fb0-94eb-cbc36587b275</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Form_test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
